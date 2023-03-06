b2334d4996e1e385656da9976ae4d20913324d12
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-03-06 11:12:44 -0500

    form config

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup = this.fb.group({
+    email   : [ '', [ Validators.email, Validators.required ] ],
+    password: [ '', [ Validators.required, Validators.minLength(6) ] ]
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formRegister: FormGroup = this.fb.group({
+    name    : [ '', [ Validators.required ] ],
+    email   : [ '', [ Validators.email, Validators.required ] ],
+    password: [ '', [ Validators.required, Validators.minLength(6) ] ]
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
 
   ngOnInit(): void {
   }
